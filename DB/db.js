9335499a231ec230271aed509542c95b5d1bcc87
Sharath Shankar V <sharathshankar4@gmail.com>
2025-02-22 15:58:41 +0530

    git ignore update

--- DB/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Database connected successfully');
-});
